bce33192acb90eb7c64d5ecbabeb20a9aea6d8ca
Alexander John <alexjohnpersonals@gmail.com>
2022-07-04 01:52:13 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/checkpoint_2_14.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkpoint_2_14.dir/main.cpp.o"
+  "checkpoint_2_14"
+  "checkpoint_2_14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checkpoint_2_14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Display my initials
+#include <iostream>
+using namespace std;
+
+int main() {
+    char first, middle, last;
+
+    first = 'A';
+    middle = 'J';
+    last = 'J';
+
+    cout << "My first initial is " << first << ", my middle initial is ";
+    cout << middle << ", and my last initial is " << last << "." << endl;
+
+    return 0;
+}
